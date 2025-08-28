b754d283af97f968432d0954a015e5511f9a66dd
Megha619801-creator <mahatomahi2062@gmail.com>
2025-08-28 23:36:56 +0300

    assignment

---
+++ week2-fe-lab5/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer';
+import MainContent from './MainContent';
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      App
+      <Header />
+      <Footer />
+      <MainContent />
+    </div>
+  )
+}
+
+export default App;
